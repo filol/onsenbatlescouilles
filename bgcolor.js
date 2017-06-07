2ab4ba46df0168d4b7379c39499db420e8ebe8a7
Slamjack <murlouk@gmail.com>
2017-06-07 22:50:07 +0200

    Create bgcolor.js

---
+++ bgcolor.js
@@ -0,0 +1,18 @@
+var previous = -1;
+
+function setRandomBackgroundColor()
+{
+	var index = Math.floor(Math.random() * 7);
+
+
+	if (index == previous)
+		index = (index + 1) % 7;
+	previous = index;
+	
+	var colors = ["#1abc9c","#2ecc71","#3498db","#9b59b6","#f1c40f","#e67e22","#e74c3c"]
+
+	document.body.style.background = colors[index];
+	var rota = Math.floor(Math.random() * 60) - 30;
+	document.getElementById('rotatingText').style.transform="rotate(" + rota + "deg)"
+	setTimeout(setRandomBackgroundColor, 375);
+}
