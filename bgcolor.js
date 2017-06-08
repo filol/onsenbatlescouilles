a5dfdaca1384b1d5a3d41f0ebffbc43aa294c9d8
Slamjack <murlouk@gmail.com>
2017-06-08 15:03:27 +0200

    Lower text angle

--- bgcolor.js
+++ bgcolor.js
@@ -12,7 +12,7 @@ function setRandomBackgroundColor()
 	var colors = ["#1abc9c","#2ecc71","#3498db","#9b59b6","#f1c40f","#e67e22","#e74c3c"]
 
 	document.body.style.background = colors[index];
-	var rota = Math.floor(Math.random() * 60) - 30;
+	var rota = Math.floor(Math.random() * 30) - 15;
 	document.getElementById('rotatingText').style.transform="rotate(" + rota + "deg)"
 	setTimeout(setRandomBackgroundColor, 375);
 }
